a0d88762d091b1f8d62971fe2c93827bcf44ac4a
cadrewj <cadrewj@users.noreply.github.com>
2019-04-01 18:29:24 +0800

    Stage 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
+import './Todos';
+import Todos from './Todos';
 
 class App extends Component {
+  state={ //this the content templete of the content you want to put on the page
+    todos:[
+      {id: 1, content: "Hire a ninja"},
+      {id: 2, content: "Find a ninja"}
+    ]
+  };
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo =>{ //used to filter and delete if they arent equal
+      return(todo.id!==id);
+    });
+    this.setState({
+      todos
+    })
+
+    
+    console.log(id);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <br/>
+          <a className="App-link" href="index.html" target="_blank" rel="noopener noreferrer">Ninjas</a>
         </header>
+        <div className="todo-app container">{/* "container" is used to put the padding around thee app*/ }
+          <h1 className="center blue-text">Todo's</h1>
+          <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        </div>
       </div>
     );
   }
---
+++ src/Todos.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Todos = ({todos,deleteTodo}) => {
+    const todoList = todos.length ? ( //if yes
+        todos.map(todo => {
+            return(
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={() => {deleteTodo(todo.id)}}> {todo.content}</span>
+                </div>
+            );
+        })
+
+    ) : 
+    
+    (//else do the folowing
+    <p className="center">No Todos Left</p>
+    );
+    return ( //used to output the todo list
+        <div className="todos collection">
+            {todoList}
+        </div>
+
+    );
+}
+export default Todos; //export so it can be used later in the app
